9a7a18487177f8b1d5a3866a5753208ae09f7885
Olamide-047 <olamideakerele39@gmail.com>
2026-03-20 19:10:38 -0700

    scroll

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Layout({ children }) {
   return (
     <div className="bg-white text-gray-800">
+      <ScrollToTop />
       <Header />
       {children}
       <Footer />
